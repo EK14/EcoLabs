db69cc2bc6622c05da58324824581e4c69e75597
Elina Karapetian <elinakarapetyan01@yandex.ru>
2024-05-05 23:54:00 +0300

    no message

---
+++ Eco.Lab1/Eco.Lab1/AssemblyFiles/Mac/Xcode_12_3/Build/Intermediates.noindex/Eco.Lab1.build/Debug/Dynamic.build/Objects-normal/x86_64/CEcoLab1.d
@@ -0,0 +1,17 @@
+dependencies: \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/SourceFiles/CEcoLab1.c \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/IEcoSystem1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/IEcoBase1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/depend.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/byteswap.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/atomic.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1MemExt.h \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/HeaderFiles/CEcoLab1.h \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/SharedFiles/IEcoLab1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IdEcoMemoryManager1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryManager1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryAllocator1.h \
+  ../../../Components/SharedFiles/IEcoCalculatorY.h \
+  ../../../Components/SharedFiles/IEcoCalculatorX.h \
+  ../../../Components/SharedFiles/IdEcoCalculatorA.h
---
+++ Eco.Lab1/Eco.Lab1/AssemblyFiles/Mac/Xcode_12_3/Build/Intermediates.noindex/Eco.Lab1.build/Debug/Dynamic.build/Objects-normal/x86_64/CEcoLab1EnumConnections.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/AssemblyFiles/Mac/Xcode_12_3/CEcoLab1EnumConnections.c \
+  /Users/elina/Desktop/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/IEcoBase1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/depend.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/byteswap.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/atomic.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1MemExt.h \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/AssemblyFiles/Mac/Xcode_12_3/CEcoLab1EnumConnections.h \
+  /Users/elina/Developer/HSE/Lab1_architecture/Eco.Lab1/Eco.Lab1/AssemblyFiles/Mac/Xcode_12_3/IEcoEnumConnections.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.List1/SharedFiles/IdEcoList1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.List1/SharedFiles/IEcoList1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.Core1/SharedFiles/IEcoSystem1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IdEcoMemoryManager1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryManager1.h \
+  /Users/elina/Desktop/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryAllocator1.h
